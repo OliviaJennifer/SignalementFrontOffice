e144464bb37d5a29dac8f2edddbe76d775720a2f
OliviaJennifer <80876456+OliviaJennifer@users.noreply.github.com>
2022-02-27 11:21:35 +0100

    c

--- office/src/components/osm-provider.js
+++
@@ -1,6 +0,0 @@
-export default{
-    maptiler: {
-        url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-        attribution: "&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-    },
-}
